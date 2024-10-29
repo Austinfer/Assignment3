8b74021d5893656420d4aa674b987194d6f1d5da
Austin Ferguson <Aferguson2213@conestogac.on.ca>
2024-10-29 14:11:00 -0400

    Add project files.

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+extern "C" char* rockPaperScissors(const char* player1, const char* player2);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestPlayer1Wins)
+		{
+			char* result = rockPaperScissors("Rock", "Scissors");
+			Assert::AreEqual("Player1", result); // Player1 should win this case
+		}
+
+		TEST_METHOD(TestPlayer2Wins)
+		{
+			char* result = rockPaperScissors("Rock", "Paper");
+			Assert::AreEqual("Player2", result); // Player2 should win this case
+		}
+
+		TEST_METHOD(TestDraw)
+		{
+			char* result = rockPaperScissors("Scissors", "Scissors");
+			Assert::AreEqual("Draw", result); // This should end in a draw
+		}
+
+		TEST_METHOD(TestInvalidInput)
+		{
+			char* result = rockPaperScissors("Rock", "InvalidInput");
+			Assert::AreEqual("Invalid", result); // This should return Invalid
+		}
+	};
+}
